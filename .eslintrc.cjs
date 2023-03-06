66918334f0da4196da68c3a009634367a1b636f3
Aleksandr <zheltkevich42@gmail.com>
2023-03-06 12:35:43 +0300

    Config linters

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -6,8 +6,18 @@ module.exports = {
         'eslint:recommended',
         'plugin:vue/vue3-recommended',
     ],
+    overrides: [
+        {
+          'files': ['*.html'],
+          'processor': 'vue/.vue'
+        }
+    ],
     rules: {
         // override/add rules settings here, such as:
-        // 'vue/no-unused-vars': 'error'
+        'vue/no-unused-vars': 'error',
+        'quotes': ['error', 'single'],
+
+        // vue/html
+        'vue/html-quotes': ['error', 'double'],
     }
 }
