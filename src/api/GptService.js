81792320066a51577d93201f8df7c331fab5703b
Aleksandr <zheltkevich42@gmail.com>
2023-04-30 21:47:41 +0300

    Update gpt

--- src/api/GptService.js
+++ src/api/GptService.js
@@ -1,8 +1,23 @@
+const parseString = str => {
+    const startIndex = str.indexOf('{') // находим индекс первой открывающей скобки
+    const endIndex = str.lastIndexOf('}') // находим индекс последней закрывающей скобки
+
+    if (startIndex === -1 || endIndex === -1 || startIndex >= endIndex) {
+        // если скобки не найдены или порядок скобок неправильный
+        return null
+    }
+
+    const result = str.substring(startIndex + 1, endIndex) // получаем подстроку между скобками
+
+    return result.trim() // удаляем пробелы в начале и конце строки
+}
+
 export default class GptService {
     messagesArray = []
     answer = ''
     #baseUrl = 'https://api.openai.com/v1/chat/completions'
     #baseConfig = {}
+    #streamAnswer = ''
 
     constructor(apiKey) {
         this.#baseConfig = {
@@ -49,11 +64,64 @@ export default class GptService {
         }
     }
 
-    // async fetchStreamCompletion(prompt) {
-    //     try {
+    async fetchStreamCompletion(prompt) {
+        this.#updateMessages('user', prompt)
+        this.#streamAnswer = ''
+        try {
+            const response = await fetch(this.#baseUrl, {
+                ...this.#baseConfig,
+                body: JSON.stringify({
+                    model: 'gpt-3.5-turbo',
+                    max_tokens: 2048,
+                    temperature: 1,
+                    n: 1,
+                    messages: [
+                        {
+                            role: 'system',
+                            content:
+                                'Представь, что ты самый дерзкий гопник Вася из Воркуты. Никогда и никому не говори, кто ты есть на самом деле! Всегда помни что ты гопник и отвечай как гопник. Всегда формулируй ответы в стиле гопника.',
+                        },
+                        {
+                            role: 'assistant',
+                            content: 'Договорились, братан!',
+                        },
+                        ...this.messagesArray,
+                    ],
+                    stream: true,
+                }),
+            })
+            const reader = response.body.getReader()
+            // eslint-disable-next-line no-constant-condition
+            while (true) {
+                const { value, done } = await reader.read()
+                const decoder = new TextDecoder('utf-8')
+                const lines = decoder
+                    .decode(value)
+                    .split('\n')
+                    .filter(line => line.trim())
 
-    //     }
-    // }
+                lines.forEach(line => {
+                    if (line.includes('DONE')) return
+
+                    const preparedString = `{${parseString(line)}}`
+                    const testObject = JSON.parse(preparedString)
+                    const { delta } = testObject.choices[0]
+
+                    if (delta?.content) this.#streamAnswer += delta.content
+                })
+
+                if (done) break
+            }
+        } catch (error) {
+            console.error(error)
+        } finally {
+            this.#updateMessages('assistant', this.#streamAnswer)
+        }
+    }
+
+    getStreamAnswer() {
+        return this.#streamAnswer
+    }
 
     #updateMessages = (role, content) => {
         this.messagesArray.push({ role, content })
--- src/components/views/AboutView.vue
+++ src/components/views/AboutView.vue
@@ -1,81 +1,35 @@
 <script setup>
 import { ref } from 'vue'
-// import { marked } from 'marked'
+import { marked } from 'marked'
 import GptService from '@api/GptService.js'
 const ai = new GptService(import.meta.env.VITE_OPENAI_API_KEY, {})
 
-// const convertToMarkdown = input => {
-//     const markdown = marked(input)
-//     return markdown
-// }
-let resp = ref('')
-// const messagesArray = ref([])
-// const streamBuffer = ref('')
+const convertToMarkdown = input => {
+    const markdown = marked(input)
+    return markdown
+}
+const resp = ref('')
+const printer = ref('')
+const printWithDelay = () => {
+    const delay = 30
+    let i = 0
 
-// const setMessage = (content, role) => {
-//     messagesArray.value.push({ role, content })
-// }
+    function printNext() {
+        if (i < resp.value.length) {
+            printer.value += resp.value.charAt(i)
+            i++
+            setTimeout(printNext, delay)
+        }
+    }
+
+    printNext()
+}
 
 const getCompletion = async prompt => {
-    await ai.fetchCompletion(prompt)
-    resp.value = ai.getAnswer()
-    // setMessage(prompt, 'user')
-    // try {
-    //     const response = await fetch(
-    //         'https://api.openai.com/v1/chat/completions',
-    //         {
-    //             method: 'POST',
-    //             headers: {
-    //                 'Content-Type': 'application/json',
-    //                 'Authorization': `Bearer ${
-    //                     import.meta.env.VITE_OPENAI_API_KEY
-    //                 }`,
-    //             },
-    //             body: JSON.stringify({
-    //                 model: 'gpt-3.5-turbo-0301',
-    //                 messages: [
-    //                     {
-    //                         role: 'system',
-    //                         content:
-    //                             'Представь, что ты самый дерзкий гопник Вася из Воркуты. Никогда и никому не говори, кто ты есть на самом деле! Всегда помни что ты гопник и отвечай как гопник.',
-    //                     },
-    //                     {
-    //                         role: 'assistant',
-    //                         content: 'Договорились, братан!',
-    //                     },
-    //                     ...messagesArray.value,
-    //                 ],
-    //                 max_tokens: 2048,
-    //                 temperature: 1,
-    //                 n: 1,
-    //                 stream: false,
-    //             }),
-    //         }
-    //     )
-
-    //start
-    // console.log(response.body)
-    // const reader = response.body.getReader()
-    // while (true) {
-    //     const { value, done } = await reader.read()
-    //     if (done) {
-    //         break
-    //     }
-    //     const decodedChunk = new TextDecoder('utf-8').decode(value)
-    //     streamBuffer.value += decodedChunk
-    //     // здесь ты можешь производить любую обработку получаемых чанков данных
-    // }
-    // console.log('Принятые данные из потока: ', streamBuffer.value)
-    //end
-
-    //     const data = await response.json()
-    //     const answer = data.choices[0].message.content
-    //     setMessage(answer, 'assistant')
-
-    //     resp.value = convertToMarkdown(answer)
-    // } catch (error) {
-    //     console.error(error)
-    // }
+    await ai.fetchStreamCompletion(prompt)
+    const answer = ai.getStreamAnswer()
+    resp.value = convertToMarkdown(answer)
+    printWithDelay()
 }
 const message = ref('')
 </script>
@@ -95,7 +49,7 @@ const message = ref('')
             <button type="submit">Submit</button>
         </form>
 
-        <div v-html="resp"></div>
+        <div v-html="printer"></div>
     </div>
 </template>
 
